3b50c7392138d6885a6a9994d92ec1b23c592d93
jenniferlazo-unah <88951754+jenniferlazo-unah@users.noreply.github.com>
2021-12-06 20:51:39 -0600

    update profesores

---
+++ resources/views/profesores/profesores_update.blade.php
@@ -0,0 +1,170 @@
+@extends('layouts.app')
+@section('title', 'Profesor')
+@section('content')
+
+    <div class="container">
+        <div class="row" style="margin-top: 15px">
+            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab"
+                 style="margin: 0 auto">
+
+                <div class="col" style="margin: 0 auto">
+                    <ul class="list-group" style="width: 33rem; margin: 0 auto">
+                        <li class="list-group-item" style="background-color:#151E6D">
+                           <h2 style="color:#ffffff; text-align: center">
+                                ACTUALIZAR PROFESOR
+                            </h2>
+                            <hr style="border-top: 2px solid white; border-bottom: 2px solid white; border-left:none; border-right:none; height: 2px; ">
+
+
+                            <div class="col-md-11" style="margin: 0 auto">
+                                <form id="form_proveedores" style="margin: 0 auto" enctype="multipart/form-data"
+                                      action="{{route("profesor.edit",["id"=>$profesor->id])}}"
+                                      method="post">
+                                    @csrf
+
+                                    <div class="d-grid gap-2" style="margin: 0 auto">
+                                            <label style="color: white"><strong>Nombres:</strong></label>
+                                            <input class="form-control  @error('nombres') is-invalid @enderror"
+                                                   placeholder=""
+                                                   onkeypress="return f_letra(event);"
+                                                   pattern="[A-Za-záéíóúñÑ ]{2,50}"
+                                                   required
+                                                   @if(old("nombres"))
+                                               value="{{old("nombres")}}"
+                                               @else
+                                               value="{{$profesor->nombres}}"
+                                               @endif
+                                                   maxlength="50" name="nombres" id="nombres">
+                                            @error('nombres')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong style="color: white">{{ $message }}</strong>
+                                            </span>
+                                            @enderror
+                                    </div>
+
+                                    <div class="d-grid gap-2" style="margin: 0 auto">
+                                        <label style="color: white"><strong>Apellidos:</strong></label>
+                                        <input class="form-control  @error('apellidos') is-invalid @enderror"
+                                               placeholder=""
+                                               onkeypress="return f_letra(event);"
+                                               pattern="[A-Za-záéíóúñÑ ]{2,50}"
+                                               required
+                                               @if(old("apellidos"))
+                                               value="{{old("apellidos")}}"
+                                               @else
+                                               value="{{$profesor->apellidos}}"
+                                               @endif
+                                               maxlength="50" name="apellidos" id="apellidos">
+                                        @error('apellidos')
+                                        <span class="invalid-feedback" role="alert">
+                                                <strong style="color: white">{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+
+                                    <div class="d-grid gap-2" style="margin: 0 auto">
+                                            <label style="color: white"><strong>Profesion:</strong></label>
+                                            <input class="form-control"
+                                                      required
+                                                      @if(old("profesion"))
+                                                      value="{{old("profesion")}}"
+                                                      @else
+                                                      value="{{$profesor->profesion}}"
+                                                     @endif
+                                                      maxlength="50"
+                                                      id="profesion" name="profesion"
+                                                      
+                                            ></input>
+                                            @error('apellidos')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong style="color: white">{{ $message }}</strong>
+                                            </span>
+                                            @enderror
+                                        </div>
+
+
+                                    <!-- <div class="d-grid gap-2" style="margin: 0 auto">
+                                        <label style="color: white"><strong>Identidad:</strong></label>
+                                        <input class="form-control  @error('identidad') is-invalid @enderror"
+                                               placeholder=""
+                                               required
+                                               value="{{old("identidad")}}"
+                                               maxlength="50" name="identidad" id="identidad">
+                                        @error('identidad')
+                                        <span class="invalid-feedback" role="alert">
+                                                <strong style="color: white">{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div> -->
+
+                                    <div class="d-grid gap-2" style="margin: 0 auto">
+                                        <label style="color: white"><strong>Estado:</strong></label>
+                                            <select class="form-select" aria-label="" name="estado" id="estado" 
+                                            @if(old("activo"))
+                                                      value="{{old("activo")}}"
+                                                      @else
+                                                      value="{{$profesor->activo}}"
+                                                     @endif>
+                                            <option selected value ="Seleccionar">Seleccionar..</option>
+                                                <option value ="Activo">A</option>
+                                                <option value="inactivo">I</option>
+                                                
+                                            </select>
+                                        @error('ciudad')
+                                        <span class="invalid-feedback" role="alert">
+                                                <strong style="color: white">{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+
+                                    <div class="d-grid gap-2" style="margin: 0 auto">
+                                    <label style="color: white"><strong>Fecha:</strong></label>
+                                    <input size="16" type="" placeholder="aa/mm/dd" class="form-control" id="fecha" name="fecha"  
+                                    
+                                    @if(old("fecha_entrada"))
+                                                      value="{{old("fecha_entrada")}}"
+                                                      @else
+                                                      value="{{$profesor->fecha_entrada}}"
+                                                     @endif>
+                                    
+                                    </div>
+
+                                    <br>
+                                    <div class="d-grid gap-2" style="margin: 0 auto">
+                                        <button id="btnRegister" class="btn btn-primary">Guardar</button>
+                                    </div>
+                                    <br>
+                                </form>
+                            </div>
+
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <script>
+            function valNumero(evt) {
+                var code = (evt.which) ? evt.which : evt.keyCode;
+                if (code == 8) {
+                    return true;
+                } else if (code >= 48 && code <= 57) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            function f_letra(evt) {
+                var code = (evt.which) ? evt.which : evt.keyCode;
+                if (code == 8 || code == 32) {
+                    return true;
+                } else if (code >= 65) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        </script>
+
+@endsection
+@yield('@Copyrigth ')
